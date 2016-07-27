c1529e53913d6a6d7bd060bcc4fd28b6800dfbb7
hsalmeida <hsalmeida@gmail.com>
2016-07-27 11:08:17 -0300

    arquivos iniciais

---
+++ js/app/x-com.js
@@ -0,0 +1,3 @@
+angular.module("x-com", [
+    'ui.router', 'ngResource', 'mongolabResourceHttp', 'ngAnimate', 'ui.bootstrap', 'ngMessages'
+]).constant('MONGOLAB_CONFIG', {API_KEY: 'YXgR-q92vuVCKlSm-ji3nplDTE7rHIQh', DB_NAME: 'xcom'});
