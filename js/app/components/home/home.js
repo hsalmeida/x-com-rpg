b044da646528fc58bf7266d5a31172a91fca7ac6
hsalmeida <hsalmeida@gmail.com>
2016-07-29 00:18:27 -0300

    tentativa da base e adicao de lista

--- js/app/components/home/home.js
+++ js/app/components/home/home.js
@@ -2,19 +2,32 @@ angular.module('x-com').controller('HomeController', ['$scope', '$rootScope', '$
     'Campaigns',
     function ($scope, $rootScope, $state, Users, $cookies, Campaigns) {
 
+        $scope.campanha = {
+            "engenheiros": 0,
+            "cientistas": 0,
+            "elerium": 0,
+            "energia": 0,
+            "max": 10,
+            "ligas": 0,
+            "intel": 0,
+            "dinheiro": 0,
+            "income": 0
+        };
+
         $scope.initHome = function () {
             waitingCircular.show();
             $scope.isAdmin = $rootScope.currentUser.admin;
             var qc = {
-
+                "users": $rootScope.currentUser._id.$oid
             };
             Campaigns.query(qc).then(function (campanhas) {
-               if(campanhas[0]) {
-
-               }
+                if (campanhas[0]) {
+                    $scope.campanha = campanhas[0];
+                }
+                waitingCircular.hide();
             });
 
-            waitingCircular.hide();
+
         };
 
         $scope.globe = function () {
