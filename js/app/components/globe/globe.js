7a3e7b90b59524755ea85ad4ceaa7255ae9f52af
hsalmeida <hsalmeida@gmail.com>
2016-07-29 17:37:38 -0300

    globe

--- js/app/components/globe/globe.js
+++ js/app/components/globe/globe.js
@@ -2,6 +2,12 @@ angular.module('x-com').controller('GlobeController', ['$scope', '$rootScope', '
     function ($scope, $rootScope, $state, Users, $cookies) {
 
         $scope.initGlobe = function () {
-
+            mapboxgl.accessToken = 'pk.eyJ1IjoiaHNhbG1laWRhIiwiYSI6ImNpZ3h2dm10MzB1cTk0bW0zaGc0MXAzMGsifQ.FhWDwRrJqmRUNRhDX1K3vg';
+            $scope.xcommap = new mapboxgl.Map({
+                container: 'xcomMap',
+                center: [-122.420679, 37.772537],
+                style: 'mapbox://styles/mapbox/dark-v9',
+                zoom: 3
+            });
         };
     }]);
