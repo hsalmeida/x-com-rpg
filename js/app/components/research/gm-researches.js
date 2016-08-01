6957e13b12f4508eae2e89183826146ed9e88d4f
Renato <Renato@Renato-PC>
2016-07-31 22:37:29 -0300

    Dominio de recursos da base

--- js/app/components/research/gm-researches.js
+++ js/app/components/research/gm-researches.js
@@ -9,6 +9,24 @@ angular.module('x-com').controller('GmResearchesController', ['$scope', '$rootSc
             "requisitos": [],
             "custo": ""
         };
+        
+        $scope.resources = {
+        	alloy: 0,
+        	intel: 0,
+        	engineers: 0,
+        	scientist: 0,
+        	supplies: 0,
+        	elerium: 0,
+        	aliens: {
+        		sectoid: 0,
+        		sectoidCommander: 0,
+        		drone: 0,
+        		muton: 0,
+        		mutonElite: 0,
+        		berserker: 0
+        	}
+        }
+        
         $scope.initGmResearch = function () {
             $scope.pesquisas = [];
         };
