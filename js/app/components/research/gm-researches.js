4ee8980e70e5b9dd7c60409120c324b4424c471f
hsalmeida <hsalmeida@gmail.com>
2016-07-31 21:42:11 -0300

    adicao do escopo de mestre

--- js/app/components/research/gm-researches.js
+++ js/app/components/research/gm-researches.js
@@ -1,6 +1,36 @@
 angular.module('x-com').controller('GmResearchesController', ['$scope', '$rootScope', '$state', 'Users', '$cookies',
-    function ($scope, $rootScope, $state, Users, $cookies) {
+    '$modal',
+    function ($scope, $rootScope, $state, Users, $cookies, $modal) {
+        $scope.pesquisa = {
+            "nome": "",
+            "proxima": "",
+            "anterior": "",
+            "requisitos": []
+        };
         $scope.initGmResearch = function () {
+            $scope.pesquisas = [];
+        };
+
+        $scope.novaPesquisa = function () {
+            $modal.open({
+                templateUrl: 'views/research/new-gm-research.html',
+                controller: 'NewGmResearchController',
+                resolve: {
+                    parentScope: function () {
+                        return $scope;
+                    }
+                }
+            }).result.then(function () {
+                    $scope.reload();
+                }, function () {
+                });
+        };
+    }]);
+
+
+angular.module('x-com').controller('NewGmResearchController', ['$scope', '$rootScope', 'parentScope',
+    function ($scope, $rootScope, parentScope) {
+        $scope.initNovaPesquisa = function () {
 
         };
     }]);
