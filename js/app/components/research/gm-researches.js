f2f5fa9404de6054bd9613380100ad37cd33853e
hsalmeida <hsalmeida@gmail.com>
2016-08-02 16:43:56 -0300

    atualizacao no gm research

--- js/app/components/research/gm-researches.js
+++ js/app/components/research/gm-researches.js
@@ -1,45 +1,22 @@
 angular.module('x-com').controller('GmResearchesController', ['$scope', '$rootScope', '$state', 'Users', '$cookies',
-    '$modal', 'Researches', '$filter',
-    function ($scope, $rootScope, $state, Users, $cookies, $modal, Researches, $filter) {
+    '$modal', 'Researches', '$filter', 'DefaultObjects',
+    function ($scope, $rootScope, $state, Users, $cookies, $modal, Researches, $filter, DefaultObjects) {
         $scope.filtraPesquisa = function (input) {
             var filtro = $filter('filter')($scope.pesquisas, input, function (a, b) {
-                if(a && a._id) {
+                if (a && a._id) {
                     return a._id.$oid === b;
                 }
             });
-            if(filtro.length > 0) {
+            if (filtro.length > 0) {
                 return filtro[0].name
             } else {
                 return "";
             }
         };
 
-        $scope.pesquisa = {
-            "name": "",
-            "description": "",
-            "cost": {},
-            "prerequisite": "",
-            "unlockResearch": "",
-            "unlockItems": [],
-            "unlockFacilities": ""
-        };
-        
-        $scope.resources = [
-            {name: "Alloy", total: 0},
-            {name: "Intel", total: 0},
-            {name: "Engineers", total: 0},
-            {name: "Scientists", total: 0},
-            {name: "Supplies", total: 0},
-            {name: "Elerium", total: 0},
-            {name: "Allies", types: [
-                    {name: "Sectoid", total: 0},
-                    {name: "Sectoid Commander", total: 0},
-                    {name: "Drone", total: 0},
-                    {name: "Muton", total: 0},
-                    {name: "Muton Elite", total: 0}
-                ]}
-        ];
-        
+        $scope.pesquisa = DefaultObjects.researchObj;
+        $scope.resources = DefaultObjects.resourceObj;
+
         $scope.initGmResearch = function () {
             Researches.all().then(function (pesquisas) {
                 $scope.pesquisas = pesquisas;
@@ -50,7 +27,7 @@ angular.module('x-com').controller('GmResearchesController', ['$scope', '$rootSc
         $scope.novaPesquisa = function () {
             $modal.open({
                 templateUrl: 'views/research/new-gm-research.html',
-                controller: 'NewGmResearchController',
+                controller: 'NewGmResearchController'
             }).result.then(function () {
                     $scope.reload();
                 }, function () {
@@ -60,18 +37,52 @@ angular.module('x-com').controller('GmResearchesController', ['$scope', '$rootSc
 
 
 angular.module('x-com').controller('NewGmResearchController', ['$scope', '$rootScope',
-    'Researches',
-    function ($scope, $rootScope, Researches) {
-        $scope.novaPesquisa = {
-            "name": "",
-            "description": "",
-            "cost": "",
-            "prerequisite": "",
-            "unlockResearch": "",
-            "unlockItems": [],
-            "unlockFacilities": ""
+    'Researches', 'DefaultObjects',
+    function ($scope, $rootScope, Researches, DefaultObjects) {
+        $scope.newResearch = DefaultObjects.researchObj;
+        $scope.resources = DefaultObjects.resourceObj;
+        $scope.tempcost = {};
+        $scope.tempPreReq = {};
+        $scope.tempUnlockRes = {};
+
+        $scope.addPreReq = function () {
+            if($scope.tempPreReq) {
+                if($.inArray($scope.tempPreReq, $scope.newResearch.prerequisite) === -1) {
+                    $scope.newResearch.prerequisite.push($scope.tempPreReq);
+                }
+            }
         };
+
+        $scope.removePreReq = function (index) {
+            $scope.newResearch.prerequisite.splice(index, 1);
+        };
+
+        $scope.addNextRes = function () {
+            if($scope.tempUnlockRes) {
+                if($.inArray($scope.tempUnlockRes, $scope.newResearch.unlockResearch) === -1) {
+                    $scope.newResearch.unlockResearch.push($scope.tempUnlockRes);
+                }
+            }
+        };
+
+        $scope.removeNextRes = function (index) {
+            $scope.newResearch.unlockResearch.splice(index, 1);
+        };
+
+        $scope.addCost = function () {
+            if($scope.tempcost) {
+                if($.inArray($scope.tempcost, $scope.newResearch.cost) === -1) {
+                    $scope.newResearch.cost.push($scope.tempcost);
+                }
+            }
+        };
+
+        $scope.removeCost = function (index) {
+            $scope.newResearch.cost.splice(index, 1);
+        };
+
         $scope.initNovaPesquisa = function () {
+            $scope.newResearch = DefaultObjects.researchObj;
             Researches.all().then(function (pesquisas) {
                 $scope.pesquisas = pesquisas;
             });
--- js/app/x-com-services.js
+++ js/app/x-com-services.js
@@ -1,14 +1,71 @@
 angular.module("x-com")
-    .factory('Users',function($mongolabResourceHttp){
+    .factory('Users', function ($mongolabResourceHttp) {
         return $mongolabResourceHttp('users');
     })
-    .factory('Campaigns',function($mongolabResourceHttp){
+    .factory('Campaigns', function ($mongolabResourceHttp) {
         return $mongolabResourceHttp('campaigns');
     })
-    .factory('Researches',function($mongolabResourceHttp){
+    .factory('Researches', function ($mongolabResourceHttp) {
         return $mongolabResourceHttp('researches');
     })
-    .factory('Items',function($mongolabResourceHttp){
+    .factory('Items', function ($mongolabResourceHttp) {
         return $mongolabResourceHttp('items');
     })
-;
+    .factory('DefaultObjects', function () {
+        return {
+            "resourceObj": [
+                {name: "Alloy", total: 0, type: "Comum"},
+                {name: "Intel", total: 0, type: "Comum"},
+                {name: "Engineers", total: 0, type: "Comum"},
+                {name: "Scientists", total: 0, type: "Comum"},
+                {name: "Supplies", total: 0, type: "Comum"},
+                {name: "Elerium", total: 0, type: "Comum"},
+                {name: "Sectoid", total: 0, type: "Alien"},
+                {name: "Sectoid Commander", total: 0, type: "Alien"},
+                {name: "Thin Man", total: 0, type: "Alien"},
+                {name: "Viper", total: 0, type: "Alien"},
+                {name: "Outsider", total: 0, type: "Alien"},
+                {name: "Faceless", total: 0, type: "Alien"},
+                {name: "Floater", total: 0, type: "Alien"},
+                {name: "Archon", total: 0, type: "Alien"},
+                {name: "Drone", total: 0, type: "Alien"},
+                {name: "Muton", total: 0, type: "Alien"},
+                {name: "Muton Elite", total: 0, type: "Alien"},
+                {name: "Berseker", total: 0, type: "Alien"},
+                {name: "Andromedon", total: 0, type: "Alien"},
+                {name: "Chryssalid", total: 0, type: "Alien"},
+                {name: "Codex", total: 0, type: "Alien"},
+                {name: "Gatekeeper", total: 0, type: "Alien"},
+                {name: "Avatar", total: 0, type: "Alien"},
+                {name: "Ethereal", total: 0, type: "Alien"},
+                {name: "Ethereal King", total: 0, type: "Alien"},
+                {name: "Viper King", total: 0, type: "Alien"},
+                {name: "Berseker Queen", total: 0, type: "Alien"},
+                {name: "Archon King", total: 0, type: "Alien"},
+                {name: "Sectopod", total: 0, type: "Alien"},
+                {name: "Mechtoid", total: 0, type: "Alien"},
+                {name: "Seeker", total: 0, type: "Alien"},
+                {name: "EXALT Operative", total: 0, type: "Alien"},
+                {name: "EXALT Medic", total: 0, type: "Alien"},
+                {name: "EXALT Heavy", total: 0, type: "Alien"},
+                {name: "EXALT Sniper", total: 0, type: "Alien"},
+                {name: "EXALT Elite Operative", total: 0, type: "Alien"},
+                {name: "EXALT Elite Medic", total: 0, type: "Alien"},
+                {name: "EXALT Elite Heavy", total: 0, type: "Alien"},
+                {name: "EXALT Elite Sniper", total: 0, type: "Alien"},
+                {name: "ADVENT Trooper", total: 0, type: "Alien"},
+                {name: "ADVENT Officer", total: 0, type: "Alien"},
+                {name: "ADVENT Stun Lancer", total: 0, type: "Alien"},
+                {name: "ADVENT Shieldbearer", total: 0, type: "Alien"}
+            ],
+            "researchObj": {
+                "name": "",
+                "description": "",
+                "cost": [],
+                "prerequisite": [],
+                "unlockResearch": [],
+                "unlockItems": [],
+                "unlockFacilities": []
+            }
+        };
+    });
